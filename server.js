307a62a56926151d339a14c8a7e76c6c852bc64f
Viraj1280 <36643898+Viraj1280@users.noreply.github.com>
2018-03-05 20:23:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,6 +50,20 @@ var articles = {
 
 
 
+app.get('/test-db',function(req,res){
+   //make a select Request and return a response 
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err,toString());
+       }
+       else{
+           res.send(JSON.stringify(result));
+       }
+       
+   });
+});
+
+
 
 function createTemplate(data) {
 
