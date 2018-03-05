3c6500e409a367a70d780bf6f6f80ebb58e206a3
Viraj1280 <36643898+Viraj1280@users.noreply.github.com>
2018-03-05 20:11:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config ={
+    user:'professionalvish',
+    database:'professionalvish',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
 
 var app = express();
 
@@ -37,6 +47,22 @@ var articles = {
 }
 
 };
+
+var pool = new Pool(config);
+
+app.get('/test-db',function(req,res){
+   //make a select Request and return a response 
+   pool.query("SELECT * FROM test",function(err,result){
+       if(err){
+           res.status(500).send(err,toString());
+       }
+       else{
+           res.send(JSON.stringify(result));
+       }
+       
+   });
+});
+
 
 
 function createTemplate(data) {
